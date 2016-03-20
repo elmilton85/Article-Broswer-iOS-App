83812f1527a3fa3a04e5daa863fb574e5601c132
Mike Fan <Mike@yingweis-macbook-pro.local>
2016-03-20 19:13:58 -0400

    nothing

---
+++ Artist/Class/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  Artist
+//
+//  Created by Yingwei Fan on 3/16/16.
+//  Copyright © 2016 Yingwei Fan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+
+@end
+
+@implementation ViewController
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
